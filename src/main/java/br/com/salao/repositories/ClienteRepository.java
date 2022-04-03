3f4167e60d4bdf88f8a88747c9e828ff69497955
M4TH3US17 <matheusdalvino50@gmail.com>
2022-04-03 18:46:41 -0400

    ClienteService

--- src/main/java/br/com/salao/repositories/ClienteRepository.java
+++ src/main/java/br/com/salao/repositories/ClienteRepository.java
@@ -7,5 +7,4 @@ import br.com.salao.entidades.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, String> {
-
 }
---
+++ src/main/java/br/com/salao/services/ClienteService.java
@@ -0,0 +1,59 @@
+package br.com.salao.services;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Service;
+
+import br.com.salao.entidades.Cliente;
+import br.com.salao.repositories.ClienteRepository;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repository;
+
+	public Page<Cliente> findByPagination(Pageable pageable) {
+		return repository.findAll(pageable);
+	}
+
+	public Cliente findByLogin(String login) {
+		if (repository.existsById(login) == false) {
+
+		}
+		return repository.getById(login);
+	}
+
+	@Transactional
+	public Cliente save(Cliente obj) {
+		return repository.save(obj);
+	}
+
+	@Transactional
+	public void deleteByLogin(String login) {
+		repository.deleteById(login);
+	}
+
+	@Modifying
+	@Transactional
+	public Cliente update(String login, Cliente obj) {
+		if (repository.existsById(login) == false) {
+
+		}
+		Cliente entity = repository.getById(login);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(Cliente entity, Cliente obj) {
+		entity.setContato(obj.getContato());
+		entity.setEmail(obj.getEmail());
+		entity.setSenha(obj.getSenha());
+		entity.setFoto(obj.getFoto());
+	}
+	
+}
